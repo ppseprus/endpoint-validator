e5104a37a9e521ace9f76359d0013428c621d573
ppseprus <p.seprus@gmail.com>
2016-06-24 20:57:12 +0200

    beta version updated

--- endpoint-validator.js
+++ endpoint-validator.js
@@ -1,30 +1,89 @@
 // input
 var inspection = [
 	{
-		alias: 'Index',
-		url: 'http://index.hu',
-		interval: 5000
-	}, {
-		alias: 'Google',
-		url: 'http://google.com',
-		interval: 2500
+		alias: 'Alias',
+		url: 'http://domain.com',
+		interval: 1000,
+		scenarios: [
+			{
+				// UNABLE TO CONNECT
+				statusCode: 0,
+				schema: {}
+			}, {
+				// SUCCESS
+				statusCode: 200,
+				schema: {
+					expectedProperty: 'expectedType'
+				}
+			}
+		]
 	}
 ];
 
 
 // requirements
 var request = require('request');
+var _ = require('lodash');
 
 
 // functionality
-function run(element, index, array) {
+function endpointValidator(element) {
 	var interval = setInterval( () => {
-	    request(element.url, function (error, response, body) {
-	        console.log(`HTTP Status Code for ${element.alias} is ${response.statusCode}`);
+	    request(element.url, function (error, header, response) {
+	    	var message = `Validation of ${element.alias} `;
+
+
+	    	if (!_.isUndefined(header) && !_.isUndefined(header.statusCode)) {
+
+		        var scenario = _.find(element.scenarios, s => {
+		        	return s.statusCode === header.statusCode;
+		        });
+
+		        if (!_.isEmpty(scenario.schema)) {
+		        	message += `with HTTP Status Code ${header.statusCode} is `;
+
+		        	try {
+		        		var responseObject = JSON.parse(response);
+		        		var errorCount = 0;
+			        	var errorWith = [];
+
+			        	_.forEach(scenario.schema, (type, key) => {
+			        		if (!responseObject.hasOwnProperty(key) || typeof responseObject[key] !== type) {
+								errorCount += 1;
+								errorWith.push(key);
+			        		}
+			        	});
+
+			        	message += `finished with `;
+			        	if (0 < errorCount) {
+			        		message += `${errorCount} error(s).\n`;
+			        		message += `The following attribute(s) are incorrect:\n`;
+			        		message += `${errorWith.toString()}`;
+			        	} else {
+			        		message += `SUCCESS.`;
+			        	}
+
+		        	} catch(err) {
+		        		// ERROR
+		        		message += `IMPOSSIBLE, due to unparsable response object.`;
+		        	}
+		        	
+		        } else {
+		        	// ERROR
+		        	message += `IMPOSSIBLE, due to missing response schema.`;
+		        }
+
+	        } else {
+	        	// ERROR
+	        	message += `IMPOSSIBLE, due to missing HTTP Status Code.`;
+	        }
+
+
+	        console.log(message + `\n`);
 	    });
 	}, element.interval);
 }
 
 
 // run
-inspection.forEach(run);
+inspection.forEach(endpointValidator);
