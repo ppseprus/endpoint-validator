a06147cbee75bba8ecfc2f59185a40b2dd61e4ef
ppseprus <p.seprus@gmail.com>
2016-06-24 19:18:03 +0200

    start multiple validations

--- endpoint-validator.js
+++ endpoint-validator.js
@@ -1,11 +1,30 @@
-var inspection = {
-    url: 'index.hu',
-    interval: 5000
-};
+// input
+var inspection = [
+	{
+		alias: 'Index',
+		url: 'http://index.hu',
+		interval: 5000
+	}, {
+		alias: 'Google',
+		url: 'http://google.com',
+		interval: 2500
+	}
+];
 
+
+// requirements
 var request = require('request');
-var interval = setInterval( () => {
-    request('http://' + inspection.url, function (error, response, body) {
-        console.log(`URL Status Code: ${response.statusCode}`);
-    });
-}, inspection.interval);
+
+
+// functionality
+function run(element, index, array) {
+	var interval = setInterval( () => {
+	    request(element.url, function (error, response, body) {
+	        console.log(`HTTP Status Code for ${element.alias} is ${response.statusCode}`);
+	    });
+	}, element.interval);
+}
+
+
+// run
+inspection.forEach(run);
