f1ce1c0b6b89d49bef26744786aadb8e23db2e4b
ppseprus <p.seprus@gmail.com>
2016-06-26 19:35:42 +0200

    services are now listed in command line and imported dynamically

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 (function() {
 	'use strict';
 
-	var validator = require('./validator'),
+	var _ = require('lodash'),
+		validator = require('./validator'),
 		endpoints = require('./endpoints'),
+		services = [];
 
-		// require slack service
-		slack = require('./services/slack');
+	_.forEach(process.argv, (service, index) => {
+		if (1 < index) {
+			try {
+				services.push(require(`./services/${service}`));
+			} catch(err) {
+				console.log(`No service found with the name ${service}.`);
+			}
+		}
+	});
 
-
-	validator(endpoints, [slack]);
+	validator(endpoints, services);
 
 })();
