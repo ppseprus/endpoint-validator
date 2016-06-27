1651190c75dd664537eb112c653c7fe2e07ef1ec
Csaba Valyi <csaba_valyi@epam.com>
2016-06-27 11:19:39 +0200

    returning a function after configuring the name

--- services/slack.js
+++ services/slack.js
@@ -8,45 +8,47 @@
 		request = require('request'),
 		settings = require('./../settings');
 
-	module.exports = function(healthObject, username) {
-		var alert, color, messageObject;
+	module.exports = function (username)  {
+		return function(healthObject) {
+			var alert, color, messageObject;
+
+			if (/^[12]..$/.test(healthObject.HTTPStatusCode) && healthObject.isConsistent) {
+				// 1xx Informational
+				// 2xx Success
+				alert = false;
+				color = 'good';
+			} else if (/^.$/.test(healthObject.HTTPStatusCode) && healthObject.isConsistent) {
+				// 3xx Redirection
+				alert = true;
+				color = 'warning';
+			} else {
+				alert = true;
+				color = 'danger';
+			}
 
 
-		if (/^[12]..$/.test(healthObject.HTTPStatusCode) && healthObject.isConsistent) {
-			// 1xx Informational
-			// 2xx Success
-			alert = false;
-			color = 'good';
-		} else if (/^.$/.test(healthObject.HTTPStatusCode) && healthObject.isConsistent) {
-			// 3xx Redirection
-			alert = true;
-			color = 'warning';
-		} else {
-			alert = true;
-			color = 'danger';
-		}
-
-
-		messageObject = JSON.stringify({
-			username: _.isString(username) ? username : USERNAME,
-			attachments: [{
-				color: color,
-				text: healthObject.log,
-				mrkdwn_in: ['text']
-			}]
-		});
-
-		if (!_.isUndefined(messageObject) && (
-				settings.FORCE_ALERT_ON_SUCCESS	// globally forced alerting on success
-				|| alert						// alert when needed
-			)
-		) {
-			request({
-				method: 'POST',
-				uri: REQUEST_URL,
-				body: messageObject
+			messageObject = JSON.stringify({
+				username: _.isString(username) ? username : USERNAME,
+				attachments: [{
+					color: color,
+					text: healthObject.log,
+					mrkdwn_in: ['text']
+				}]
 			});
-		}
-	};
+
+			if (!_.isUndefined(messageObject) && (
+					settings.FORCE_ALERT_ON_SUCCESS	// globally forced alerting on success
+					|| alert						// alert when needed
+				)
+			) {
+				request({
+					method: 'POST',
+					uri: REQUEST_URL,
+					body: messageObject
+				});
+			}
+		};
+	}
+
 
 })();
