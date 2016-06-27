193557f964763d6c9d17fd32dc833919fd8fd04b
ppseprus <p.seprus@gmail.com>
2016-06-28 00:26:13 +0200

    yup options added to settings.js

--- settings.js
+++ settings.js
@@ -2,6 +2,12 @@
 	'use strict';
 
 	module.exports = Object.freeze({
+		YUP_OPTIONS: {
+			strict: true,
+			abortEarly: false,
+			stripUnknown: false,
+			recursive: true
+		},
 		DEFAULT_REPEAT_INTERVAL: 1000 * 60 * 10, // 10 minute default
 		ENDPOINT_INTERVAL_PATTERN: /^(\d+)([smhd])?$/,
 		MARKDOWN_CHARACTERS: /[_~*`]/gm,
